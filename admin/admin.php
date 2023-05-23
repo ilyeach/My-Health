48d7ee3011b7c3591e55a44be2fde72aaac1dfa9
ilyeach <ilyeach121212@gmail.com>
2023-05-23 19:39:05 +0530

    add project

--- admin/admin.php
+++ admin/admin.php
@@ -1,1 +1,24 @@
-admin
+<!doctype html>
+<html lang="en">
+ <head>
+
+ <?php include('head.php'); ?>
+<title>Login</title>
+
+</head>
+<body>
+  <div id="header">
+	<?php include('header.php'); ?>
+  </div>
+
+
+<h1>WELCOME ADMIN</h1>
+
+
+</body>
+
+	
+	<div id="footer">
+<?php include('footer.php'); ?>
+ </div>
+</html>

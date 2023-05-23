b9945a186178ef6d2ee4656aa488c2b8d15f1b17
ilyeach <ilyeach121212@gmail.com>
2023-05-23 18:44:07 +0530

    add admin file

---
+++ login.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icon.css">
+
+<body>
+<section class="ftco-section">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-md-6 text-center mb-5">
+<h2 class="heading-section">Login</h2>
+</div>
+</div>
+<div class="row justify-content-center">
+<div class="col-md-12 col-lg-10">
+<div class="wrap d-md-flex">
+<div ><img class="navbar-brand" src="images/doc.jpg" width="400" height="430"/>
+</div>
+<div class="login-wrap p-4 p-md-5">
+<div class="d-flex">
+<div class="w-100">
+<h3 class="mb-4">Sign In</h3>
+</div>
+</div>
+<form action="#" class="signin-form">
+<div class="form-group mb-3">
+<label class="label" for="name">Username</label>
+<input type="text" class="form-control" placeholder="Username" required="">
+</div>
+<div class="input-group mb-3">
+<input id="pass_log_id" type="password" class="form-control" placeholder="Password" required="">
+<button class="btn btn-outline-secondary" type="button" id="button-addon2"><span toggle="#password-field" class="fa fa-fw fa-eye field_icon toggle-password"></span></button>
+
+</div>
+<div class="form-group">
+<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+</div>
+<div class="form-group d-md-flex">
+<div class="w-50 text-left">
+<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+<input type="checkbox" checked="">
+<span class="checkmark"></span>
+</label>
+</div>
+<div class="w-50 text-md-right">
+<a href="#">Forgot Password</a>
+</div>
+</div>
+</form>
+<p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+</body>
+
+	
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>

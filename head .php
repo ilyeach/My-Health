858ba3ca80cc1d16f8f2f48ae7bc3f784cc25a14
ilyeach <ilyeach121212@gmail.com>
2023-07-06 01:13:25 +0530

    add css file

---
+++ head .php
@@ -0,0 +1,48 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../library/css/bootstrap.min.css">
+	
+	<link rel="stylesheet" href="../library/css/fonts/font-awesome.min.css">
+     <script src="../library/js/jbootstrap.min.js" ></script>
+
+<link rel="stylesheet" href="../library/css/3.4.1 bootstrap.min.css">
+<link rel="stylesheet" href="../library/css/fonts/font-awesome 5.15.3.css" />
+<link href="../library/css/Material+Icons.css" rel="stylesheet">
+<link rel="stylesheet" href="../library/css/4.5.0 bootstrap.min.css">
+<link rel="stylesheet" href="../library/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="../library/css/fonts/font-awesome 5.15.4.min.css">
+<link rel="stylesheet" href="../library/css/Open+Sans.css">
+<link rel="stylesheet" href="../library/css/4.7.0 font-awesome.min.css">
+<link rel="stylesheet" href="../library/css/3.3.7 bootstrap.min.css">
+<link href="../library/css/4.1.1 bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" href="../library/js/jquery-ui.css>
+
+ 
+
+
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Include jQuery UI library -->
+<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
+
+<!-- Include jQuery UI CSS -->
+
+
+<!-- Include jQuery UI library -->
+<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
+
+<!-- Include jQuery UI CSS -->
